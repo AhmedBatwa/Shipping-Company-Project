21316146102e49a419856cf53c1d8568869f7e40
nhiane <41923791+Nhian@users.noreply.github.com>
2020-11-08 23:18:04 +0300

    implementing Sender

--- sender/Sender.java
+++ sender/Sender.java
@@ -5,10 +5,23 @@ import java.util.ArrayList;
 import shipment.Shipment;
 public abstract class Sender {
 	ArrayList<Shipment> shipments= new ArrayList<Shipment>();
-	
+	 
 	
 	public void addShipment(Shipment shipment) {
 		shipments.add(shipment);
 	}
 	
+	
+	
+
+	
+	
 }
+
+
+
+
+
+
+
+
