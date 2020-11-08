31fd6933fec7b43e4fd736129ff13e2348110942
Sulaiman <sulaiman_samonah@Hotmail.com>
2020-11-08 16:08:59 +0300

    added preferred delivery times for all types of receivers, added code to Receive method please take a look

--- receiver/Receiver.java
+++ receiver/Receiver.java
@@ -3,22 +3,31 @@ package receiver;
 import java.util.ArrayList;
 
 import shipment.Shipment;
+import shipment.Status;
 
 public abstract class Receiver {
 	
 	private ArrayList<Shipment> shipments;
 	private int[] preferredDeliveryTime;
-	
-	
 	public Receiver(int[] preferredDeliveryTime) {
 		shipments = new ArrayList<Shipment>();
 		this.preferredDeliveryTime=preferredDeliveryTime;
 	}
-	public boolean Recieve(Shipment shipment) {
-		
-		return true;
-	}
-
+	public boolean Receive(Shipment shipment) {
+		boolean bool=false;
+		for (int i:getPreferredDeliveryTime()) {
+			if(shipment.getPrefferedDeliveryTime()[i]==getPreferredDeliveryTime()[i]) {
+				shipment.setStatus(Status.DELIVERED);
+				bool=true;
+				break;
+			}
+			else{
+				shipment.setStatus(Status.DELIVERY_FAILED);
+				bool=false;
+			}	
+			}
+		return bool;
+		}
 	public ArrayList<Shipment> getShipments() {
 		return shipments;
 	}
@@ -34,14 +43,5 @@ public abstract class Receiver {
 	
 	
 }
-
-
-/*TODO
- * get familiar with Git basic commands 
- * fields must be private
- * discuss the import from other packages
- * the commenting methods
- * no objects created from abstract classes
- */
 
 
--- shipment/ECompanySales.java
+++ shipment/ECompanySales.java
@@ -2,6 +2,6 @@ package shipment;
 
 public class ECompanySales extends Shipment{
 	public ECompanySales() {
-		super(new int[] {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1});
+		super(new int[] {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,});
 	}
 }
