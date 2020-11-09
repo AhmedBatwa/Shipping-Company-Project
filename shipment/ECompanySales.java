542e7a24365213807a7208bee3e9bd3f2db4b6f9
AIB <ah444mad@hotmail.com>
2020-11-09 16:16:50 +0300

    Added toString methods along with shipment ID and counter

--- shipment/ECompanySales.java
+++ shipment/ECompanySales.java
@@ -3,8 +3,12 @@ import sender.Sender;
 import receiver.Receiver;
 
 public class ECompanySales extends Shipment{
+	// Constructors
 	public ECompanySales(Sender sender,Receiver receiver) {
 		super(sender, receiver);
 		super.setPrefferedDeliveryTime(new int[] {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1});
 	}
+	
+	// Print
+	public String toString() {return String.format("%s | Shimpent Type: %s | Shipment Status: %s", super.toString(), "E Company Sales", getStatus());}
 }
--- shipment/Food.java
+++ shipment/Food.java
@@ -3,27 +3,25 @@ import sender.Sender;
 import receiver.Receiver;
 
 public class Food extends Shipment implements Undelayable{
-	
-	
-	private static final int shelfLife=2;
 	/**
-	 * food has limited shelf life assumed to be 2 hours here
-	 *
+	 * food has limited shelf life assumed to be 2 hours.
 	 */
+	private static final int shelfLife=2;
+	
+	// Constructors
 	public Food(Sender sender , Receiver receiver,int hour) {
 		super(sender, receiver);
 		init(hour);
 	}
 	
+	// Set preferred delivery time
 	public void init(int hour){
 		int[] PrefferedDeliveryTime= new int[24];
-	for(int i=hour;i<shelfLife ; i++) {
-		PrefferedDeliveryTime[i]=1;
-	}
-	setPrefferedDeliveryTime(PrefferedDeliveryTime);
-	
+		for(int i=hour;i<shelfLife ; i++) {PrefferedDeliveryTime[i]=1;}
+		setPrefferedDeliveryTime(PrefferedDeliveryTime);
 	}
 	
-	
+	// Print
+	public String toString() {return String.format("%s | Shimpent Type: %s | Shipment Status: %s", super.toString(), "Food", getStatus());}
 }
 	
